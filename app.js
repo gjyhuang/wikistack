f753b2cd938e881fb110caf4c57c8b4127b5182e
Marcos Santanna <test_123@TEST-123s-MacBook-Air.local>
2019-09-26 12:54:03 -0400

    added validators

--- app.js
+++ app.js
@@ -2,18 +2,34 @@ const express = require("express");
 const morgan = require("morgan");
 const main = require("./views/main")
 
+const  models  = require('./models');
+
 const app = express();
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: false }));
 
+// db.authenticate().
+// then(() => {
+//   console.log('connected to the database');
+// })
+
 app.get('/', (req, res, next) => {
   res.send(main())
 })
+const initDb = async () =>{
+  
+  await models.db.sync({force: true});
 
-const PORT = 3000;
+  
+  const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+
+} 
+initDb()
+
+
